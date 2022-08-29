0f938f7d2825b1cecaf55b145a6d1cedde45ceb8
kenizaya <harveylstark@gmail.com>
2022-08-29 07:13:20 +0530

    Check if start and end becomes equal

--- knightsTravails.js
+++ knightsTravails.js
@@ -9,8 +9,19 @@ const knightMoves = (start, end) => {
     [2, -1],
     [2, 1],
   ]
+
+  const visited = []
+  const queue = [[start, end]]
+  let pos = null
+
+  while (queue.length) {
+    pos = queue.shift()
+    if (pos[0][0] === pos[1][0] && pos[0][1] === pos[1][1]) return
+  }
 }
 
 const isValid = (x, y) => {
   return !(x < 0 || x >= 8 || y < 0 || y >= 8)
 }
+
+knightMoves([3, 3], [3, 3])
