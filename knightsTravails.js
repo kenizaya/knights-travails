5ae7d295f8c6092dbcb89afeba66cc07537f6de5
kenizaya <harveylstark@gmail.com>
2022-08-30 06:57:19 +0530

    Calculate total moves

--- knightsTravails.js
+++ knightsTravails.js
@@ -1,3 +1,11 @@
+class Move {
+  constructor(x, y, moves) {
+    this.x = x
+    this.y = y
+    this.moves = moves
+  }
+}
+
 const knightMoves = (start, end) => {
   const knightOffsets = [
     [-2, -1],
@@ -10,18 +18,38 @@ const knightMoves = (start, end) => {
     [2, 1],
   ]
 
-  const visited = []
-  const queue = [start]
+  const visited = new Set()
+  const queue = [new Move(start[0], start[1], 0)]
   let pos = null
+  let xNew = null
+  let yNew = null
 
-  while (queue.length) {
+  while (queue.length > 0) {
     pos = queue.shift()
-    if (pos[0][0] === end[0][0] && pos[0][1] === end[0][1]) console.log(pos)
+    if (pos.x === end[0] && pos.y === end[1]) {
+      console.log('You made it in ' + pos.moves + ' moves')
+      return pos.moves
+    }
+
+    if (!visited.has(pos)) {
+      visited.add(pos)
+      for (let offset of knightOffsets) {
+        xNew = pos.x + offset[0]
+        yNew = pos.y + offset[1]
+        // console.log(xNew, yNew)
+        if (isValid(xNew, yNew)) {
+          // console.log(xNew, yNew)
+          queue.push(new Move(xNew, yNew, pos.moves + 1))
+          // console.log(queue)
+        }
+      }
+    }
   }
+  return -1
 }
 
 const isValid = (x, y) => {
-  return !(x < 0 || x >= 8 || y < 0 || y >= 8)
+  return x >= 0 && x < 8 && y >= 0 && y < 8
 }
 
-knightMoves([3, 3], [3, 3])
+console.log(knightMoves([0, 7], [7, 0]))
